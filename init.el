fb20a406f1d2f085dd54a70e540181668aeafd45
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2025-08-19 17:56:52 +0900

    update copilot
    Signed-off-by: Yutaka Kondo <yutaka.kondo@youtalk.jp>

--- init.el
+++ init.el
@@ -204,7 +204,7 @@
                      )))
 (use-package htmlize)
 (use-package copilot
-  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("dist" "*.el"))
   :ensure t)
 (add-hook 'prog-mode-hook 'copilot-mode)
 (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
